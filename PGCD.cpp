6e08825ebe764c5cbce53d1360531db4c15a5a05
ecendales <ecendales@gmail.com>
2020-03-10 16:05:28 -0400

    creation de project

--- PGCD.cpp
+++ PGCD.cpp
@@ -3,7 +3,38 @@
 #include<iostream>
 using namespace std;
 int main() {
+	int num[100];
+	int den[100];
+	int res[100];
+	int n, i, a, b;
 
+	do {
+		cout << "Entrez le nombre de fractions" << endl;
+		cin >> n;
+
+	} while (n <= 0);
+	for (int i = 0; i < n; i++) {
+
+		cout << "Entrez le numérateur de fraction #" << i + 1 << " : " << endl;
+		cin >> num[i];
+		cout << "Entrez le dénominateur de fraction #" << i + 1 << " : " << endl;
+		cin >> den[i];
+		a = __max(num[i], den[i]);
+		b = __min(num[i], den[i]);
+
+		do {
+			res[i] = b;
+			b = a % b;
+			a = res[i];
+
+		} while (b != 0);
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		cout << "\nEntre (" << num[i] << " et " << den[i] << ") EL PGCD est: " << res[i] << "  simplification " << num[i] / res[i] << "/" << den[i] / res[i];
+
+	}
 
 
 	return 0;
